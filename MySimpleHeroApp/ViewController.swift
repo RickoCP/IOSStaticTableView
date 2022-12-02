f7e30001a705c967fab5d7b4bfb9c84f6bbc67f2
ricko <ricko.caesar.putra@gmail.com>
2022-12-02 17:22:48 +0700

    Initial Commit

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by ricko caesar putra on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
